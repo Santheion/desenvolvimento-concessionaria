9230bfde36acbe9b4dcf53973c7e9944e11ebac3
Douglas Silva Oliveira <douglas.silva22@hotmail.com>
2020-08-26 19:46:34 -0300

    first commit

---
+++ Concessionaria.Web/Controllers/CarroController.cs
@@ -0,0 +1,24 @@
+﻿using Concessionaria.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Concessionaria.Web.Controllers
+{
+    public class CarroController : Controller
+    {
+        // GET: Carro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DadosCarro dadosCarro)
+        {
+            return View("RegistroCarro", dadosCarro);
+        }
+    }
+}
---
+++ Concessionaria.Web/Models/DadosCarro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Concessionaria.Web.Models
+{
+    public class DadosCarro
+    {
+        [DisplayName("Nome do carro: ")]
+        [Required]
+        [MaxLength(80, ErrorMessage = "Nome do carro deve possuir no maximo 80 caracteres")]
+        [MinLength(3, ErrorMessage = "Nome do carro deve possuir no minimo 3 caracteres")]
+        public string NomeCarro { get; set; }
+        public string Cor { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+    }
+}
